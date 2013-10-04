0047361b6826f07f3c139ad5ae7f70a9a0e38d87
mivianmarques <mivianmarques@f813f357-5dfe-6bfa-e651-bb48b3012a42>
2013-10-04 21:49:24 +0000

    gerarimagem

--- trunk/Change Explorer/src/changeexplorer/GerarImagem.java
+++ trunk/Change Explorer/src/changeexplorer/GerarImagem.java
@@ -1,54 +1,36 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package changeexplorer;
 
-import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Rectangle;
-import java.awt.TexturePaint;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+import java.awt.*;
 import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 
-import javax.imageio.ImageIO;
-import javax.swing.JPanel;
+public class GerarImagem extends javax.swing.JPanel {
+	BufferedImage imagem;
+	Rectangle2D retangulo;
 
-/**
- *
- * @author Mivian
- */
-public class GerarImagem extends JPanel {
+	public GerarImagem(String nomeImagem) {
+		try {
+			imagem = ImageIO.read(getClass().getResourceAsStream(
+					"src\\imagens" + nomeImagem));
 
-	BufferedImage imagem;  
-    Rectangle2D retangulo;
-	
-    public GerarImagem(String nomeImagem) {
-    	try {  
-            //se você pegar uma imagem dentro do mesmo jar/projeto  
-           imagem = ImageIO.read(new File("src\\imagens\\"+nomeImagem));                   
-           
-           //retangulo = new Rectangle(0,0,130,130);                                   
-             
-       } catch (IOException ex) {  
-    	   
-    	   System.out.println("NAO ACHEI CAMINHO!!");
-    	   System.out.println(System.getProperty("user.dir"));
-    	   ex.printStackTrace(System.err);  
-       }  
-    }
+		} catch (IOException ex) {
+			ex.printStackTrace(System.err);
+		}
+	}
 
-    public void paintComponent(Graphics g){    
-        
-    	retangulo = new Rectangle(0,0,this.getWidth(),this.getHeight());  
-          
-          
-        TexturePaint p = new TexturePaint(imagem,retangulo);  
-        Graphics2D g2 = (Graphics2D) g;  
-        g2.setPaint(p);  
-        g2.fillRect(0,0,this.getWidth(),this.getHeight());  
+	@Override
+	public void paintComponent(Graphics g) {
 
-    }
+		retangulo = new Rectangle(0, 0, this.getWidth(), this.getHeight());
+
+		TexturePaint p = new TexturePaint(imagem, retangulo);
+		Graphics2D g2 = (Graphics2D) g;
+		g2.setPaint(p);
+		g2.fillRect(0, 0, this.getWidth(), this.getHeight());
+
+	}
 }
